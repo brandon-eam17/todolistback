0788cf1eb2fcc0fb051867cfba9099b196e07c15
brandon <0701722@utla.com>
2025-12-10 15:38:00 -0600

    todolist backend fv

--- api/views.py
+++ api/views.py
@@ -5,35 +5,42 @@ from django.shortcuts import get_object_or_404
 from .models import Todo
 from .serializers import TodoSerializer
 
-@api_view({'GET', 'POST'})
+@api_view(['GET', 'POST'])
 def todo_list(request):
+
+    # Obtener todos los elementos
     if request.method == 'GET':
         todos = Todo.objects.all()
         serializer = TodoSerializer(todos, many=True)
         return Response(serializer.data)
-    
+
+    # Crear un nuevo elemento
     if request.method == 'POST':
-        todos = Todo.objects.all()
-        if(serializer.is_valid()):
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
-    
-@api_view({'GET', 'PUT', 'DELETE'})
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def todo_detail(request, pk):
     todo = get_object_or_404(Todo, pk=pk)
 
+    # Obtener un solo elemento
     if request.method == 'GET':
         serializer = TodoSerializer(todo)
         return Response(serializer.data)
-    
+
+    # Actualizar el elemento
     elif request.method == 'PUT':
         serializer = TodoSerializer(todo, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
-    
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # Eliminar el elemento
     elif request.method == 'DELETE':
         todo.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
